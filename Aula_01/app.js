ad6472506614aaace48f82d9a84b5df24195a9d3
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-05-22 19:04:12 -0300

    feat: learning about var, let, const and comment

--- Aula_01/app.js
+++ Aula_01/app.js
@@ -1,1 +1,21 @@
-alert('Hello world!')
+let age = 23
+let currentYear = 2022
+
+console.log('age: ', age, currentYear)
+
+age = 24
+console.log('Age is now: ', age)
+
+const name = 'Gueller'
+
+var car = {
+  color: 'blue',
+  brand: 'Ferrari',
+  model: 'x2323'
+}
+
+// commenting in one line
+
+/*
+  comenting in block
+*/
