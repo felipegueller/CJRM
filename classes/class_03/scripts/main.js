b9178bce067c6e2294f9dd8e4a0766f395136025
felipegueller <felipe.gueller@gmail.com>
2022-07-19 16:23:05 -0300

    :tada: feat: Learnig more about arrays

---
+++ classes/class_03/scripts/main.js
@@ -0,0 +1,27 @@
+let heroes = ['Wolwerine', 'Superman', 'Super Justiceiro']
+heroes[4] = 'Fred'
+// heroes = ['new array'] // works
+
+const fruits = ['banana', 'apple', 'orange']
+fruits[3] = 'grape'
+// fruits = ['new array'] // does work, error 
+
+// Methods and property
+// console.log(`fruits length: ${fruits.length} items`) // length property
+
+const fruitsString = fruits.join(' | ')
+// console.log(fruitsString, '\n' ,typeof fruitsString)
+
+const indexOfApple = fruits.indexOf('apple')
+console.log(`Apple index: ${indexOfApple}`)
+
+const fruitsLengthAfterPushApple = fruits.push('apple')
+// console.log(fruitsLengthAfterPushApple)
+
+const lastIndexOfApple = fruits.lastIndexOf('apple')
+console.log(`Last apple's index: ${lastIndexOfApple}`)
+
+const lastElementRemovedOfArray = fruits.pop()
+console.log(lastElementRemovedOfArray)
+
+console.log('Concat: ', fruits.concat(heroes, [2, 3, 4]))
