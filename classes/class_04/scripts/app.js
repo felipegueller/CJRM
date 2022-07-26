7b32a4f3a484a1dd51a9f9d88cc55fb60017dd00
felipegueller <felipe.gueller@gmail.com>
2022-07-26 20:22:40 -0300

    :sparkles: feat: finish the fourth class

---
+++ classes/class_04/scripts/app.js
@@ -0,0 +1,46 @@
+const number = 32
+
+// console.log(number == 2)
+// console.log(number != 2)
+// console.log(number > 2)
+// console.log(number < 2)
+// console.log(number >= 2)
+// console.log(number <= 2)
+// console.log(number === '32') // false
+// console.log(number !== '32') // true
+
+const name = 'felipe'
+
+// lowercase is greather than uppercase
+
+// console.log(name == 'felipe')
+// console.log(name == 'Felipe')
+// console.log(name > 'Felipe')
+// console.log(name < 'Felipe')
+// console.log(name < 'Amélia')
+
+const email = 'felipe.gueller@gmail.com'
+
+// console.log(email.includes('@')) // true
+// console.log(email.includes('gmail')) // true
+// console.log(email.includes('gueller')) // true
+// console.log(email.includes('Gueller')) // false
+
+console.log(Boolean(10)) // true
+console.log(Number('Hello')) // NaN
+console.log(String(10)) // '10'
+
+// falsy values list: 
+/*
+  - false
+  - 0
+  - NaN
+  - ''
+  - ""
+  - ``
+  - null
+  - undefined
+*/
+
+// Truthy values 
+// Any value different of falsy's values
