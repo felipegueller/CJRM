e6b9c6dbf25559ee00165e90513170b957be82a9
felipegueller <felipe.gueller@gmail.com>
2022-07-27 17:34:53 -0300

    :sparkles: feat: adding three more samples of MDN

--- classes/stream_control/for_loop/scripts/app.js
+++ classes/stream_control/for_loop/scripts/app.js
@@ -1,6 +1,6 @@
 // basic loop
-// for (let i = 0; i <= 5; i++)
-//   console.log(`The index's value now is ${i}`)
+for (let i = 0; i <= 5; i++)
+  console.log(`The index's value now is ${i}`)
 
 // running an array
 const names = ['Ana', 'Mario', 'Steve']
@@ -9,9 +9,30 @@ let htmlList = ''
 for (let i = 0; i < names.length; i++) 
   htmlList += `<li>${names[i]}</li>\n`
 
-// console.log(htmlList)
+console.log(htmlList)
+
+// counter optional
+let i = 0
+for (; i < 3; i++)
+  console.log(i)
+
+// conditinal expression optional
+for (let i = 0; ; i++) {
+  if (i >= 3) break
+  console.log(i)
+}
+
+// none statement
+let j = 0
+
+for(;;) {
+  if (j >= 3) break
+  console.log(j)
+  j++
+}
 
 // MDN sample using two conters
 const arr = [1, 2, 3, 4, 5, 6];
+
 for (let i = 0, j = arr.length - 1; i < j; i++, j--) 
   console.log(`${arr[i]} ${arr[j]}`)
