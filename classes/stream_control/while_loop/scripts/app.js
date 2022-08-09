0ed6c4757d99963c44b442d91898fc694971d958
felipegueller <felipe.gueller@gmail.com>
2022-08-09 16:48:05 -0300

    :tada: feat: learning about while loop

---
+++ classes/stream_control/while_loop/scripts/app.js
@@ -0,0 +1,12 @@
+// Show the names using the while loop
+
+const names = ['João', 'Maria', 'José']
+let index = 0
+
+while (index < names.length) {
+  console.log(names[index])
+  index++
+}
+
+
+// 
