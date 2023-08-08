7ddee27566d4ea84b50d91381e0a8edac77b727f
Felipe Gueller <felipe.gueller@gmail.com>
2023-08-08 10:04:19 -0300

    Learning about this keyword

---
+++ classes/objects/keyword-this/app.js
@@ -0,0 +1,34 @@
+let user = {
+  name: 'Felipe',
+  age: 25,
+  email: 'felipe.gueller@gmail.com',
+  federativeUnit: 'ES',
+  blogPost: ['Falar sobre animais', 'Duolingo, aplicativo de idiomas'],
+  login: function () {
+    console.log('Usuário logado')
+  },
+  logout: function () {
+    console.log('Usuário deslogado')
+  },
+  logBlobPost () {
+    console.log(`O usuário ${this.name} escreveu os seguintes posts:`)
+
+    this.blogPost.forEach(post => console.log(post))
+  },
+  sintaxSugar () {
+    console.log('Function declaration em objetos de maneira reduzida')
+  },
+  thisFunctionDeclaration () {
+    console.log('Referencia o objeto que invoca o método: ')
+    console.log(this)
+  },
+  thisArrowFunction: () => {
+    console.log('Referencia o objeto do escopo no qual o método foi invocado: ')
+    console.log(this)
+  }
+}
+
+user.logBlobPost()
+
+user.thisArrowFunction()
+user.thisFunctionDeclaration()
