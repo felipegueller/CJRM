a70905869072a9bf83f6fe1d7977b45b49f7b584
Felipe Gueller <felipe.gueller@gmail.com>
2023-08-09 14:36:31 -0300

    Learning about Math JavaScript object

---
+++ classes/objects/math/app.js
@@ -0,0 +1,23 @@
+// Objeto Math
+console.log(Math)
+
+// Constantes
+console.log(Math.E)
+console.log(Math.LN2)
+console.log(Math.LN10)
+console.log(Math.PI)
+console.log(Math.SQRT1_2)
+console.log(Math.SQRT2)
+
+// Funções utilitárias
+console.log(Math.cbrt(27)) // raíz cubica
+console.log(Math.sqrt(4)) // raíz quadrada
+console.log(Math.ceil(4.2)) // arredonda para cima
+console.log(Math.floor(3.7)) // arredonda para baixo
+console.log(Math.round(3.5)) // arredonda conforme a matemática
+console.log(Math.trunc(3.9)) // remove a parte de decimal
+console.log(Math.abs(-5)) // aplica o valor absoluto
+
+// número aleatório
+console.log(Math.random()) // número decimal aleatório
+console.log(Math.round(Math.random() * 100)) // número aleatório entre 0 e 100
